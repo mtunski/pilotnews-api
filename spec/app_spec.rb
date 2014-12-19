f34465c2487d549b861683f3b94743321f526e4d
Mateusz Tuński <mateusz@tunski.net>
2014-12-19 13:30:04 +0100

    [#3] Implement tests reflecting the futre structure of the API

--- spec/app_spec.rb
+++ spec/app_spec.rb
@@ -6,7 +6,68 @@ describe App do
 
   it 'returns a successful response' do
     get '/'
-
     expect(last_response).to be_ok
+  end
+
+  it 'GET /api/stories returns all submitted stories' do
+    skip
+
+    get '/api/stories'
+    expect(last_response).to be_ok
+  end
+
+  it 'GET /api/stories/:id returns single story' do
+    skip
+
+    story = {}
+
+    get "/api/stories/#{story.id}"
+    expect(last_response).to be_ok
+  end
+
+  it 'POST /api/stories creates a new story' do
+    skip
+
+    story = {}
+
+    post '/api/stories', { story: story }
+    expect(last_response.status).to eq(201)
+  end
+
+  it 'PATCH /api/stories/:id updates a story' do
+    skip
+
+    story = {}
+    attributes = {}
+
+    patch "/api/stories/#{story.id}", { attributes: attributes }
+    expect(last_response.status).to eq(204)
+  end
+
+  it 'PATCH /api/stories/:id/upvote upvotes a story' do
+    skip
+
+    story = {}
+
+    patch "/api/stories/#{story.id}/upvote"
+    expect(last_response.status).to eq(204)
+  end
+
+  it 'PATCH /api/stories/:id/downvote downvotes a story' do
+    skip
+
+    story = {}
+
+    patch "/api/stories/#{story.id}/downvote"
+    expect(last_response.status).to eq(204)
+  end
+
+  it 'PATCH /api/stories/:id/unvote undoes the vote' do
+    skip
+
+    story = {}
+
+    patch "/api/stories/#{story.id}/unvote"
+    expect(last_response.status).to eq(204)
   end
 end
