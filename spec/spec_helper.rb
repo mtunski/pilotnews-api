1515601e13ca7cb6496cd3026005c7e937a14526
Mateusz Tuński <mateusz@tunski.net>
2014-12-19 18:55:37 +0100

    [#6] Use database_cleaner

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,5 +1,6 @@
 require 'dotenv'
 require 'rack/test'
+require 'database_cleaner'
 
 Dotenv.load('.env.test')
 
@@ -7,4 +8,15 @@ ENV['RACK_ENV'] = 'test'
 
 RSpec.configure do |config|
   config.include Rack::Test::Methods
+
+  config.before(:suite) do
+    DatabaseCleaner.strategy = :transaction
+    DatabaseCleaner.clean_with(:truncation)
+  end
+
+  config.around(:each) do |example|
+    DatabaseCleaner.cleaning do
+      example.run
+    end
+  end
 end
