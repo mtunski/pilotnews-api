e20a033ed1f6b039567e95cf4be4e4c5754681eb
Mateusz Tuński <mateusz@tunski.net>
2014-12-24 12:37:09 +0100

    Improve User validations

--- lib/models/user.rb
+++ lib/models/user.rb
@@ -1,4 +1,5 @@
 class User < ActiveRecord::Base
-  validates :login,    presence: true
+  validates :login,    presence: true,
+                       uniqueness: true
   validates :password, length: { minimum: 3 }
 end
