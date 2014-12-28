d0fe2ed0100c6f3d551259aef26e8272b6360f2d
Mateusz Tuński <mateusz@tunski.net>
2014-12-28 17:16:18 +0100

    Use Bcrypt to encrypt/decrypt users' passwords

--- lib/models/user.rb
+++ lib/models/user.rb
@@ -1,5 +1,18 @@
+require 'bcrypt'
+
 class User < ActiveRecord::Base
-  validates :login,    presence: true,
+  include BCrypt
+
+  validates :login,    presence:   true,
                        uniqueness: true
-  validates :password, length: { minimum: 3 }
+  validates :password, length: { minimum: 3 }, on: :create
+
+  def password
+    @password ||= Password.new(encrypted_password)
+  end
+
+  def password=(new_password)
+    @password               = Password.create(new_password)
+    self.encrypted_password = password
+  end
 end
