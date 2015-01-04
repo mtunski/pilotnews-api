cede26830c30660990e84b751a23a3323edcd703
Mateusz Tuński <mateusz@tunski.net>
2015-01-04 17:53:23 +0100

    Make password length validation work as expected

--- lib/models/user.rb
+++ lib/models/user.rb
@@ -3,16 +3,21 @@ require 'bcrypt'
 class User < ActiveRecord::Base
   include BCrypt
 
+  attr_accessor :password
+
   validates :login,    presence:   true,
                        uniqueness: true
   validates :password, length: { minimum: 3 }, on: :create
+
+  before_create :encrypt_password
 
   def password
     @password ||= Password.new(encrypted_password)
   end
 
-  def password=(new_password)
-    @password               = Password.create(new_password)
-    self.encrypted_password = password
+  private
+
+  def encrypt_password
+    @password = self.encrypted_password = Password.create(password)
   end
 end
