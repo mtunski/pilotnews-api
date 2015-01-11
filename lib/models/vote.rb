17cd1b6133500be632173108527f171b6224501b
Mateusz Tuński <mateusz@tunski.net>
2015-01-11 14:20:59 +0100

    Make it impossible for the user to vote twice for the same story

--- lib/models/vote.rb
+++ lib/models/vote.rb
@@ -3,6 +3,8 @@ class Vote < ActiveRecord::Base
   belongs_to :user
 
   validates :story, :user, presence: true
-  validates :value, inclusion: { in: [-1, 1],
-                                 message: 'Value can be either -1 (downvote) or 1 (upvote)' }
+  validates :story, uniqueness: { scope: :user,
+                                  message: 'You can vote only once' }
+  validates :value, inclusion:  { in: [-1, 1],
+                                  message: 'Value can be either -1 (downvote) or 1 (upvote)' }
 end
